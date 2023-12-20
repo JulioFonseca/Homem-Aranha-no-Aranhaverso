9e93aa0fe5ba4c8eca4149b3ad92da86038dfa64
juliofonseca <julio@softbuilder.com>
2023-12-19 22:55:24 -0300

    Update API endpoint URL

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -8,7 +8,7 @@ interface IProps {
 }
 
 async function getHeroesData(): Promise<{ data: IHeroData[] }> {
-  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
+  const res = await fetch(`https://homem-aranha-no-aranhaverso.vercel.app/api/heroes`);
 
   if (!res.ok) {
     throw new Error("Falha ao buscar heróis");
